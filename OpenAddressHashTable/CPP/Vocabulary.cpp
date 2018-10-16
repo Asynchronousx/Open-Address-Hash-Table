3926545d734c507be06b9821b0b09badb0469ec0
Asynchronousx <38207760+Asynchronousx@users.noreply.github.com>
2018-10-16 12:38:48 +0200

    Rename OpenAddressHashTable/Vocabulary.cpp to OpenAddressHashTable/CPP/Vocabulary.cpp

---
+++ OpenAddressHashTable/CPP/Vocabulary.cpp
@@ -0,0 +1,16 @@
+﻿// OpenAddressHashTable.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "class_HashTable.h"
+#include "functions.h"
+
+
+int main()
+{
+	HashMap Dictionary(16);
+	PrintProgramINFO();
+	Menu();
+	Selector(Dictionary);
+}
+
